413220873d45f30945c4e97aeea7ff4e2fc1155a
ravishangar <mirthveen@gmail.com>
2016-12-28 12:46:55 +0530

    first project

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>math calculations</title>
+</head>
+<body>
+<form>
+	<button>plus</button><br>
+	<button>minus</button><br>
+	<button>mul</button><br>
+	<button>divide</button>
+</form>
+</body>
+</html>
